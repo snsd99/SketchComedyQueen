17b41fccab4dd63012686aedad61dc0dfea54854
snsd99 <taeyeon222@pm.me>
2026-06-26 09:56:31 +0800

    Initial commit

--- app/src/main/java/com/aespa/musicdec/decrypt/activity/KugouDecActivity.java
+++ app/src/main/java/com/aespa/musicdec/decrypt/activity/KugouDecActivity.java
@@ -1,18 +1,24 @@
 package com.aespa.musicdec.decrypt.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.aespa.musicdec.R;
+import com.xuexiang.xui.widget.layout.linkage.view.LinkageRecyclerView;
 
 /**
  * KugouDecActivity: 酷狗解密的Activity。
  */
 public class KugouDecActivity extends AppCompatActivity {
+    /** KugouDecActivity中包含的可滚动组件，是一种容器。 */
+    private LinkageRecyclerView linkageRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kugou_dec);
+        this.linkageRecyclerView = findViewById(R.id.kugouRecyclerView);
     }
 }
+
